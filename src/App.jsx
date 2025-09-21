d350e64a437eb57762646befe4f836cc8c50dd38
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-21 16:29:57 +0530

    title.jsx file added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from './components/Navbar'
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
 
 const App = () => {
   const [theme, setTheme] = useState(() => {
@@ -18,6 +19,7 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy/>
+      <Services/>
     </div>
   );
 };
