4dc2353436598e4a41db6bf4002743979d9bd982
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-21 20:20:38 +0530

    ServiceCard file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
