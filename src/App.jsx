28874ad134719fab642887a3bc59eea81e5e923a
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-22 16:23:33 +0530

    smooth effect in cursor

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ const App = () => {
       <Footer theme={theme} />
 
       {/* custom cursor */}
-      <div ref={outlineRef} className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"></div>
+      <div ref={outlineRef} className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999] " style={{transition: 'transform 0.1s ease-out'}}></div>
       <div ref={dotRef} className="fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]"></div>
     </div>
   );
