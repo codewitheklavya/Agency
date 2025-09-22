6adac84025ca0156a9c2f58e389b7fc8fe15ab5f
Eklavyasingh7 <codewitheklavya@gmail.com>
2025-09-22 17:10:54 +0530

    animation in contact us form added

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast'
+import { motion } from "framer-motion"; 
 
 const ContactUs = () => {
     const onsubmit = async (event) => {
@@ -32,10 +33,20 @@ const ContactUs = () => {
     }
 }
   return (
-    <div id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+    <motion.div 
+    initial = "hidden"
+    whileInView= "visible"
+    viewport={{once: true}}
+    transition={{staggerChildren: 0.2}}
+    id='contact-us' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
         <Title title = 'Reach out to us' desc='From strategy to execution, we craft digital solutions that moves your business forward.'/>
 
-        <form onSubmit={onsubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+        <motion.form 
+                  initial = {{opacity: 0, y: 30}}
+                  whileInView={{opacity: 1, y: 0}}
+                  transition={{duration:0.4, delay: 0.4}}
+                  viewport={{once:true}}
+        onSubmit={onsubmit} className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
             <div>
                 <p className='mb-2 text-sm font-medium'>Your name</p>
                 <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
@@ -60,8 +71,8 @@ const ContactUs = () => {
             <button type="submit" className='w-max flex gap-2 bg-primary text-white text-sm px-6 py-2 mb-6 rounded-full cursor-pointer hover:scale-103 transition-all'>
                 Submit <img src={assets.arrow_icon} alt="" className='w-4'/>
             </button>
-        </form>
-    </div>
+        </motion.form>
+    </motion.div>
   )
 }
 
